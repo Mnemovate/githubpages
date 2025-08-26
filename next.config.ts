4b9fd11c1fcbbfb61ba8afe28ce5a67ace7c095f
deployrailway1 <deployrailway1@gmail.com>
2025-08-27 03:32:08 +0700

    check

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/githubpages",
+  assetPrefix: "/githubpages/",
   images: {
     unoptimized: true,
   },
